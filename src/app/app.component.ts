776ca0de7bec99247bcfc8968f211fc83e3c7db1
pushpak <pushpak.singirikonda@scoratech.com>
2018-09-07 16:37:53 +0530

    basic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Inject } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  allUsers: {}[];
+  showField = false;
+  user: any;
+  randomId: any;
+  name: any;
+  animal: any;
   itemValue = '';
   items: Observable<any[]>;
-  constructor(public db: AngularFireDatabase) {
-    console.log('ssas', this.db.list('tictacTry1').remove('LLmvTkFodFcGgt2y3vs'));
+  constructor(public db: AngularFireDatabase, public dialog: MatDialog) {
+    console.log('sa', db.list('users').valueChanges().subscribe(data => {
+      console.log('sad', data);
+      this.allUsers = data;
+    }));
+  }
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    // this.openDialog();
+  }
+  showInput() {
+    this.showField = true;
+    this.randomId = Math.floor((Math.random() * 100000) + 1);
+    this.db.list('users').push({user: this.user, randomId:  this.randomId});
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '100%',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
   onSubmit() {
     this.db.list('/items').push({ content: this.itemValue });
@@ -18,4 +50,21 @@ export class AppComponent {
   }
   // tslint:disable-next-line:member-ordering
   title = 'app';
+}
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+// tslint:disable-next-line:component-class-suffix
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogOverviewExampleDialog } from './app.component';
+import {
+  MatButtonModule, MatDialogModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DialogOverviewExampleDialog
+  ],  entryComponents: [
+    DialogOverviewExampleDialog],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
   ],
+  exports: [MatButtonModule, MatDialogModule],
   providers: [],
   bootstrap: [AppComponent]
 })
